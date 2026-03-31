3c56af30fd8caf9a926b5e1d79ec4a5dea8c8812
HayetAdwoa <149185865+HayetAdwoa@users.noreply.github.com>
2026-03-31 06:49:20 +0000

    Update index.js

--- incident-service/src/index.js
+++ incident-service/src/index.js
@@ -6,7 +6,10 @@ const incidentRoutes = require('./routes/incidents');
 const { connectRabbitMQ } = require('./mq');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://demo-frontend-busc.onrender.com', 'http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json());
 
 app.use('/incidents', incidentRoutes);
